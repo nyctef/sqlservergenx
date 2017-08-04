683897e9c3940a557f8fded2358133d214e35c26
Mark Jordan <mark.jordan@red-gate.com>
2017-08-04 15:16:14 +0100

    start figuring out how to script CREATE TABLE statements

--- sqlservergenx_f/Program.fs
+++ sqlservergenx_f/Program.fs
@@ -7,9 +7,14 @@ type ColumnType =
     | Int
     | NVarChar of CharLength
 
-type Column = {name:string; ctype:ColumnType}
+type ColumnName = string
 
-type Constraint = {name:string}
+type ObjectName = string
+let objectNameSql x = x
+
+type Column = {name:ColumnName; ctype:ColumnType}
+
+type ConstraintDefinition = {name:string}
 
 type TableType =
     | PlainTable
@@ -18,41 +23,53 @@ type TableType =
     | NodeTable
     | EdgeTable
 
-type TableProperties = {name:string; ttype:TableType}
+type TableDefinition = {name:ObjectName; ttype:TableType; columns:List<Column>}
 
 type Statement =
-    | CreateTable of TableProperties * List<Column>
-    | CreateConstraint of Constraint
+    | CreateTable of TableDefinition
+    | CreateConstraint of ConstraintDefinition
 
 type DatabaseModel = {statements: List<Statement>}
 let addStatement model statement = {model with statements=statement::model.statements}
 
-let createTableSql props colummns =
+
+///////////////////////// sql generation
+
+let columnSql column =
     "1234"
+
+let createTableSql table =
+    "CREATE TABLE " + objectNameSql table.name + "\n" +
+    "(\n" +
+    (List.map columnSql table.columns |> String.concat ",\n") +
+    ")\n"
 
 let getSql x = 
     List.map (fun statement ->
         match statement with
-            | CreateTable (props, columns) -> createTableSql props columns
+            | CreateTable table -> createTableSql table
             | CreateConstraint x -> "hello")
             x.statements
+
+
+/////////////////////////// model generation
 
 // given a seed and a database model, produce a more complicated database model
 type Complicator = int -> DatabaseModel -> DatabaseModel
 
 let addTable:Complicator = fun seed model ->
-    let tp = {name="table"; ttype=PlainTable}
-    let statement = CreateTable(tp, [{name="col1"; ctype=NVarChar(Length(5))}])
+    let tp = {name="table"; ttype=PlainTable; columns = [{name="col1"; ctype=NVarChar(Length(5))}]}
+    let statement = CreateTable(tp)
     addStatement model statement
     
 
 [<EntryPoint>]
 let main argv = 
     printfn "%A" argv
+
     let dbModel = {statements=[]}
     let dbModel = addTable 1 dbModel
     let sql = getSql dbModel
     printfn "%A" sql
-    //let dbModel = DatabaseModel{statements:
-    //    CreateTable(TableProperties{name:"hllo
+
     0 // return an integer exit code
