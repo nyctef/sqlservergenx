34199eded224229bdeddc15a5f76d7ef51fd070b
Nyctef <nyctef@nyctef.com>
2017-08-05 14:56:58 +0100

    experiment with custom fscheck gen

--- sqlservergenx_f/Tests.fs
+++ sqlservergenx_f/Tests.fs
@@ -1,16 +1,24 @@
 ﻿module Tests
 
 open NUnit.Framework
+open FsCheck
 open Code
 
 [<TestFixture>]
 type TestClass() = 
 
     [<Test>]
-    member this.scratchPad() = 
+    member this.scratchPad() =
         let dbModel = {statements=[]}
         let randGenerator () = 1
         let dbModel = addTable randGenerator dbModel
         let dbModel = addColumn randGenerator dbModel
         let sql = getSql dbModel
         printfn "%A" sql
+
+    [<Test>]
+    member this.``fscheck scratchpad``() =
+        let g = gen { return true };
+        let r = Random.StdGen (1,2)
+        let result = Gen.eval 1 r g
+        printfn "%A" result
